08bc00bf8ceab1134dc269f746b93a468f298c7f
LG <lefgiak@gmail.com>
2022-12-25 20:55:28 +0000

    asd

--- components/designs/Designs.tsx
+++ components/designs/Designs.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import projects from "@/data/content/projects";
 
 function Designs() {
   return (
@@ -51,7 +52,9 @@ function Designs() {
               </h2>
             </header>
             <div style={{ marginBottom: "2rem", display: "block" }}>
-              {Experiences.map((item) => Education(item))}
+              {Experiences.map((item) =>
+                Experiences2(item, Experiences.indexOf(item))
+              )}
             </div>
           </div>
           <div style={{ marginBottom: "2em" }}>
@@ -94,11 +97,22 @@ const Eduactions = [
 (AUTH), 2011`,
 ];
 const Experiences = [
-  `- Self Learning (Solidity/React/Typescript/Node.js), 2020`,
-  `- Blockchain Developer, National Kapodistrian Uni of Athens (99/100), 2020`,
-  `- MSc, Introduction to Digital Currencies (91/100), Uni of Nicosia, 2018`,
-  `- MSc, Aerospace Engineering, Cranfield Uni, 2013 and 2015`,
   `- TorrSwap : Smart Contracts and Integration`,
+  `- infiniTorr : Smart Contracts and Integration`,
+  `- Wonka : Smart Contracts and Integration`,
+  `- CHEDDA : Smart Contracts and Integration`,
+  `- Shiryo : Smart Contracts and Integration`,
+  `- Babynft : Smart Contracts and Integration`,
+  `- Magic : Smart Contracts and Integration`,
+];
+const Experienceslinks = [
+  `https://www.torrswap.io`,
+  `- infiniTorr : Smart Contracts and Integration`,
+  `- Wonka : Smart Contracts and Integration`,
+  `- CHEDDA : Smart Contracts and Integration`,
+  `- Shiryo : Smart Contracts and Integration`,
+  `- Babynft : Smart Contracts and Integration`,
+  `- Magic : Smart Contracts and Integration`,
 ];
 const Education = (text) => {
   return (
@@ -119,4 +133,29 @@ const Education = (text) => {
   );
 };
 
+const Experiences2 = (text, text2) => {
+  console.log(text2);
+  return (
+    <a
+      href={projects[text2].link}
+      target="_blank"
+      className="hover:bg-fun-pink hover:text-white transition-colors cursor-pointer"
+    >
+      <h3
+        key={text}
+        style={{
+          fontWeight: "700",
+          margin: "0 0 0.75rem",
+          marginBottom: "10px",
+          fontSize: "1.5rem",
+          letterSpacing: "-.05rem",
+          lineHeight: "1",
+        }}
+      >
+        {projects[text2].title}
+        <br />
+      </h3>
+    </a>
+  );
+};
 export default Designs;
