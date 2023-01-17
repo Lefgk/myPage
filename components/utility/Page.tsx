dbb9f8ddfcb39aa2f28c775811f5aa7f253f8f8e
LG <lefgiak@gmail.com>
2023-01-17 09:31:10 +0000

    asas

--- components/utility/Page.tsx
+++ components/utility/Page.tsx
@@ -18,41 +18,6 @@ function Page({ currentPage, meta: { title, desc }, children }: PageProps) {
     >
       <Head>
         <title>{pageTitle}</title>
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/static/favicon/apple-touch-icon.png"
-        />
-
-        <link rel="manifest" href="/static/favicon/site.webmanifest" />
-        <meta name="title" content={pageTitle} />
-        <meta name="description" content={desc} />
-
-        <meta property="twitter:title" content={pageTitle} />
-        <meta property="twitter:description" content={desc} />
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KC3CN7V');`,
-          }}
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                page_path: window.location.pathname,
-              });
-          `,
-          }}
-        />
       </Head>
 
       <main className="p-5 w-full flex-1 text-center">
