03232316bdc1f94f052ea68163c94603657ea1c3
LG <lefgiak@gmail.com>
2023-03-17 14:46:29 +0000

    google manager ads

--- components/utility/Page.tsx
+++ components/utility/Page.tsx
@@ -18,6 +18,19 @@ function Page({ currentPage, meta: { title, desc }, children }: PageProps) {
     >
       <Head>
         <title>{pageTitle}</title>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6X087438P9"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtag/js?id=G-6X087438P9');`,
+          }}
+        ></script>
       </Head>
 
       <main className="p-5 w-full flex-1 text-center">
